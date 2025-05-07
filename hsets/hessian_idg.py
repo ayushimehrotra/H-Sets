c631711a2b649ec776dffd0fa8ae8e836f1d8dfa
Ayushi Mehrotra <71472634+ayushimehrotra@users.noreply.github.com>
2025-05-07 16:55:00 -0700

    Update hessian_idg.py

--- hsets/hessian_idg.py
+++ hsets/hessian_idg.py
@@ -13,17 +13,17 @@ from modify_relu import replace_relu_with_modifiedrelu
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-
 class HessianIntegratedDirectionalGradients:
 
     def __init__(self, model):
         self.model = model
         self.max_features = 2000
+        self.ig = IntegratedGradients(model)
 
     def make_attribution(self, input, index, max_elements, threshold):
         model_copy = copy.deepcopy(self.model)
         replace_relu_with_modifiedrelu(model_copy)
-        current_attribution_image = np.zeros(input.detach().cpu().numpy().shape)
+        current_attribution_image = np.zeros(input.squeeze().detach().cpu().numpy().shape)
 
         def compute_hessian(i, j, k, grad_output, input, max_elements, threshold):
             if np.count_nonzero(current_attribution_image) > max_elements:
@@ -32,8 +32,13 @@ class HessianIntegratedDirectionalGradients:
             input.requires_grad_(True)
             grad2rd = torch.autograd.grad(grad_output[0, i, j, k], input, retain_graph=True)[0]
             np_grad2rd = grad2rd.detach().cpu().numpy()
+            np_grad2rd = (np_grad2rd - np_grad2rd.min()) / (np_grad2rd.max() - np_grad2rd.min() + 1e-8)  
+
+            del grad2rd
+            torch.cuda.empty_cache()
 
             np_grad2rd[np_grad2rd <= threshold] = 0
+            # print("[INFO] Number of Features", np.count_nonzero(np_grad2rd), "with threshold", threshold)
 
             if np.count_nonzero(np_grad2rd) == 0:
                 return
@@ -65,24 +70,33 @@ class HessianIntegratedDirectionalGradients:
         grad_output = torch.autograd.grad(score, input, create_graph=True)[0]
         compute_hessian(index[1], index[2], index[3], grad_output, input, max_elements, threshold)
 
+        del model_copy, input, output, score, indices, grad_output
+        gc.collect()
+        torch.cuda.empty_cache()
+
         return current_attribution_image
 
     def find_feature_interaction_sets_sam(self, input, k, threshold):
         gc.collect()
         torch.cuda.empty_cache()
 
-        sam_checkpoint = "sam_vit_h_4b8939.pth"
-        model_type = "vit_h"
-
-        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        sam = sam_model_registry["vit_h"](checkpoint="sam_vit_h_4b8939.pth")
         sam.to(device="cuda")
 
-        mask_generator = SamAutomaticMaskGenerator(sam)
+        # mask_generator = SamAutomaticMaskGenerator(sam)
+        mask_generator = SamAutomaticMaskGenerator(
+            model=sam,
+            min_mask_region_area=1000,
+        )
+
         image = input.detach().cpu().numpy()
         image = np.transpose((image / 2) + 0.5, (1, 2, 0))
         image = np.clip(image, 0, 1) * 255
         image = image.astype('uint8')
         mask = mask_generator.generate(image)
+
+        del sam, mask_generator
+        torch.cuda.empty_cache()
 
         sorted_anns = sorted(mask, key=(lambda x: x['area']), reverse=True)
         img = np.zeros((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1]))
@@ -94,12 +108,16 @@ class HessianIntegratedDirectionalGradients:
             count += 1
 
         self.model.to(device="cuda")
-        input = torch.tensor(input).to(device).unsqueeze(0)
         self.model.eval()
+
+        input = torch.tensor(input).to(device).unsqueeze(0)
         input.requires_grad_(True)
         output = self.model(input)
+
         score, indices = torch.max(output, 1)
         grad_output = torch.autograd.grad(score, input, create_graph=True)[0]
+        # grad_output = self.ig.attribute(input, target=indices)
+
         np_grad_output = grad_output.detach().cpu().numpy()
         flattened_arr = np_grad_output.flatten()
         sorted_indices = np.argsort(flattened_arr)[::-1]
@@ -110,7 +128,8 @@ class HessianIntegratedDirectionalGradients:
             indices = np.unravel_index(flat_index, grad_output.shape)
 
             if img[indices[2]][indices[3]] not in sets:
-                temp_attr = self.make_attribution(input, indices, self.max_features, grad_output[indices].item()*threshold)
+                # temp_attr = self.make_attribution(input, indices, self.max_features, grad_output[indices].item()*threshold)
+                temp_attr = self.make_attribution(input, indices, self.max_features, threshold)
                 if np.count_nonzero(temp_attr) != 0:
                     sets[img[indices[2]][indices[3]]] = temp_attr
                     # print("[INFO] Set " + str(count) + " Created with " + str(np.count_nonzero(sets[img[indices[2]][indices[3]]])))
@@ -130,7 +149,6 @@ class HessianIntegratedDirectionalGradients:
         img *= np_image
         mag = np.linalg.norm(img)
         img = img/mag
-        weight = math.comb(self.max_features, np.count_nonzero(mask))
         baseline = 0*input
         
         scaled_inputs = [baseline + (float(i)/steps)*(input-baseline) for i in range(0, steps+1)]
@@ -139,10 +157,9 @@ class HessianIntegratedDirectionalGradients:
             inp = inp.detach()
             grads.append(img*np.transpose(attributions.saliency(self.model, inp.unsqueeze(0)), (1, 2, 0)))
         idg = np.average(grads, axis=0)
-        # print(np.linalg.norm(idg), weight)
         return np.linalg.norm(idg)
 
-    def attribute(self, inputs, threshold=0.5, steps=30, k=5, max_samples=50):
+    def attribute(self, inputs, threshold=0.50, steps=30, k=5, max_samples=30):
         idgs = []
         for image in inputs:
             start_time = time.time()
@@ -154,10 +171,14 @@ class HessianIntegratedDirectionalGradients:
                 for i in range(max_samples):
                     sample_mask = mask*np.random.randint(0, 2, size=mask.shape)
                     value_set += self.attribute_set_ig(image, sample_mask)
+                # print("[INFO] Importance Score of Set with", np.count_nonzero(mask), "features:", value_set)
                 attr_set = mask*(value_set)
+                # print("[INFO] Attribution Set", attr_set)
                 final_attr.append(attr_set)
-            idg = np.sum(final_attr, axis=0)
+            # print("[INFO] Final Attribution Set", final_attr)
+            idg = np.nansum(final_attr, axis=0)
             # print("[INFO] Sets Attributed")
             idgs.append(idg)
             print(time.time() - start_time)
+        idgs = np.array(idgs, dtype=object)
         return idgs
